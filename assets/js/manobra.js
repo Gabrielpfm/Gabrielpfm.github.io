29f1bd40fad65b446aae03194b33a37abc3ed9ec
Gabriel Pascua de Freitas Moreira <gabrielpfm@al.insper.edu.br>
2019-11-29 07:43:49 -0300

    sdf

--- assets/js/manobra.js
+++ assets/js/manobra.js
@@ -1,38 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-	let test = document.querySelector('.test')
+  let iframe = document.querySelector('iframe')
+  let button = document.querySelector('button')
 
-	test.addEventListener('click', function() {
-		before()
-
-		setTimeout(after, 2000)
-	})
+  button.addEventListener('click', function() {
+    if(iframe.requestFullscreen) {
+      iframe.requestFullscreen()
+    }
+    else if(iframe.msRequestFullscreen) {
+      iframe.msRequestFullscreen()
+    }
+    else if(iframe.mozRequestFullScreen) {
+      iframe.mozRequestFullScreen()
+    }
+    else if(iframe.webkitRequestFullscreen) {
+      iframe.webkitRequestFullscreen()
+    }
+  })
 })
-
-function before() {
-
-	/* seu código aqui */
-	let hide = document.querySelector('.hidden')
-	console.log(hide)
-	hide.classList.remove('hidden')
-	let test = document.querySelector('.test')
-	test.classList.add('hidden')
-
-
-
-}
-
-
-function after() {
-	let hide = document.querySelector('.load')
-	console.log(hide)
-	hide.classList.add('hidden')
-
-	/* seu código aqui */
-
-	let test = document.querySelector('.test')
-	test.classList.remove('hidden')
-
-	/* seu código aqui */
-
-	test.innerHTML = 'conexão funcionando!'
-}
